d642f3b897dd87354a60bc1e9db17a6c7e4914cf
mcmanimie <mcmanimie@gmail.com>
2022-09-02 17:00:37 -0400

    First push

--- main/assets/js/scripts.js
+++ main/assets/js/scripts.js
@@ -257,3 +257,169 @@ Math.easeOutElastic = function (t, b, c, d) {
 function resetFocusTabsStyle() {
   window.dispatchEvent(new CustomEvent('initFocusTabs'));
 };
+// File#: _1_3d-card
+// Usage: codyhouse.co/license
+(function() {
+  var TdCard = function(element) {
+    this.element = element;
+    this.maxRotation = parseInt(this.element.getAttribute('data-rotation')) || 2; // rotation max value
+    this.perspective = this.element.getAttribute('data-perspective') || '300px'; // perspective value
+    this.rotateX = 0;
+    this.rotateY = 0;
+    this.partRotateX = 0;
+    this.partRotateY = 0;
+    this.deltaRotation = 0.3;
+    this.animating = false;
+    initTdEvents(this);
+  };
+
+  function initTdEvents(tdCard) {
+    // detect mouse hovering over the card
+    tdCard.element.addEventListener('mousemove', function(event){
+      if(tdCard.animating) return;
+      tdCard.animating = window.requestAnimationFrame(moveCard.bind(tdCard, event, false));
+    });
+
+    // detect mouse leaving the card
+    tdCard.element.addEventListener('mouseleave', function(event){
+      if(tdCard.animating) window.cancelAnimationFrame(tdCard.animating);
+      tdCard.animating = window.requestAnimationFrame(moveCard.bind(tdCard, event, true));
+    });
+  };
+
+  function moveCard(event, leaving) {
+    // get final rotation values
+    setRotationLevel(this, event, leaving);
+
+    // update rotation values
+    updateRotationLevel(this);
+  };
+
+  function setRotationLevel(tdCard, event, leaving) {
+    if(leaving) {
+      tdCard.rotateX = 0;
+      tdCard.rotateY = 0;
+      return;
+    }
+
+    var wrapperPosition = tdCard.element.getBoundingClientRect();
+    var rotateY = 2*(tdCard.maxRotation/wrapperPosition.width)*(event.clientX - wrapperPosition.left - wrapperPosition.width/2);
+    var rotateX = 2*(tdCard.maxRotation/wrapperPosition.height)*(wrapperPosition.top - event.clientY + wrapperPosition.height/2);
+
+    if(rotateY > tdCard.maxRotation) rotateY = tdCard.maxRotation;
+    if(rotateY < -1*tdCard.maxRotation) rotateY = -tdCard.maxRotation;
+    if(rotateX > tdCard.maxRotation) rotateX = tdCard.maxRotation;
+    if(rotateX < -1*tdCard.maxRotation) rotateX = -tdCard.maxRotation;
+
+    tdCard.rotateX = rotateX;
+    tdCard.rotateY = rotateY;
+  };
+
+  function updateRotationLevel(tdCard) {
+    if( (tdCard.partRotateX == tdCard.rotateX) && (tdCard.partRotateY == tdCard.rotateY)) {
+      tdCard.animating = false;
+      return;
+    }
+
+    tdCard.partRotateX = getPartRotation(tdCard.partRotateX, tdCard.rotateX, tdCard.deltaRotation);
+    tdCard.partRotateY = getPartRotation(tdCard.partRotateY, tdCard.rotateY, tdCard.deltaRotation);
+    // set partial rotation
+    rotateCard(tdCard);
+    // keep rotating the card
+    tdCard.animating = window.requestAnimationFrame(function(){
+      updateRotationLevel(tdCard);
+    });
+  };
+
+  function getPartRotation(start, end, delta) {
+    if(start == end) return end;
+    var newVal = start;
+    if(start < end) {
+      newVal = start + delta;
+      if(newVal > end) newVal = end;
+    } else if(start > end) {
+      newVal = start - delta;
+      if(newVal < end) newVal = end;
+    }
+    return newVal;
+  }
+
+  function rotateCard(tdCard) {
+    tdCard.element.style.transform = 'perspective('+tdCard.perspective+') rotateX('+tdCard.partRotateX+'deg) rotateY('+tdCard.partRotateY+'deg)';
+  };
+
+  window.TdCard = TdCard;
+
+  //initialize the TdCard objects
+  var tdCards = document.getElementsByClassName('js-td-card');
+  if( tdCards.length > 0 && Util.cssSupports('transform', 'translateZ(0px)')) {
+    for( var i = 0; i < tdCards.length; i++) {
+      (function(i){
+        new TdCard(tdCards[i]);
+      })(i);
+    }
+  };
+}());
+
+// File#: _1_main-header
+// Usage: codyhouse.co/license
+(function() {
+  var mainHeader = document.getElementsByClassName('js-header');
+  if( mainHeader.length > 0 ) {
+    var trigger = mainHeader[0].getElementsByClassName('js-header__trigger')[0],
+      nav = mainHeader[0].getElementsByClassName('js-header__nav')[0];
+
+    // we'll use these to store the node that needs to receive focus when the mobile menu is closed
+    var focusMenu = false;
+
+    //detect click on nav trigger
+    trigger.addEventListener("click", function(event) {
+      event.preventDefault();
+      toggleNavigation(!Util.hasClass(nav, 'header__nav--is-visible'));
+    });
+
+    // listen for key events
+    window.addEventListener('keyup', function(event){
+      // listen for esc key
+      if( (event.keyCode && event.keyCode == 27) || (event.key && event.key.toLowerCase() == 'escape' )) {
+        // close navigation on mobile if open
+        if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger)) {
+          focusMenu = trigger; // move focus to menu trigger when menu is close
+          trigger.click();
+        }
+      }
+      // listen for tab key
+      if( (event.keyCode && event.keyCode == 9) || (event.key && event.key.toLowerCase() == 'tab' )) {
+        // close navigation on mobile if open when nav loses focus
+        if(trigger.getAttribute('aria-expanded') == 'true' && isVisible(trigger) && !document.activeElement.closest('.js-header')) trigger.click();
+      }
+    });
+
+    // listen for resize
+    var resizingId = false;
+    window.addEventListener('resize', function() {
+      clearTimeout(resizingId);
+      resizingId = setTimeout(doneResizing, 500);
+    });
+
+    function doneResizing() {
+      if( !isVisible(trigger) && Util.hasClass(mainHeader[0], 'header--expanded')) toggleNavigation(false);
+    };
+  }
+
+  function isVisible(element) {
+    return (element.offsetWidth || element.offsetHeight || element.getClientRects().length);
+  };
+
+  function toggleNavigation(bool) { // toggle navigation visibility on small device
+    Util.toggleClass(nav, 'header__nav--is-visible', bool);
+    Util.toggleClass(mainHeader[0], 'header--expanded', bool);
+    trigger.setAttribute('aria-expanded', bool);
+    if(bool) { //opening menu -> move focus to first element inside nav
+      nav.querySelectorAll('[href], input:not([disabled]), button:not([disabled])')[0].focus();
+    } else if(focusMenu) {
+      focusMenu.focus();
+      focusMenu = false;
+    }
+  };
+}());
